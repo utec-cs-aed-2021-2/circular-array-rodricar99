1df87869798dc1a9f3d4b219a39317bf3c275a95
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2021-09-21 20:44:25 -0500

    finaly

--- stack.h
+++ stack.h
@@ -1,6 +1,26 @@
 #include "circulararray.h"
-
+#include <stack>
 template <typename T>
-class StackArray : public CircularArray<T> { 
-    
+class StackArray : public CircularArray<T> {
+private:
+    stack<T>st;
+public:
+    StackArray();
+    void push_front(T data);
+    T pop_front();
 };
+
+template<typename T>
+void StackArray<T>::push_front(T data) {
+    st.push(data);
+}
+
+template<typename T>
+T StackArray<T>::pop_front() {
+    T temp= st.top()
+    st.pop();
+    return temp;
+}
+
+template<typename T>
+StackArray<T>::StackArray() = default;
